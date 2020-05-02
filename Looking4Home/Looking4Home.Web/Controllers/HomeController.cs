4ba178dded59ba269206ad906912fc4a63af5168
javirivera1320@gmail.com <javirivera1320@gmail.com>
2020-05-02 10:24:14 -0600

    listo

--- Looking4Home/Looking4Home.Web/Controllers/HomeController.cs
+++ Looking4Home/Looking4Home.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Looking4Home.BL;
+﻿using Lookig4Home.Web.Models;
+using Looking4Home.BL;
+using Pagination;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -10,8 +12,8 @@ namespace Looking4Home.Web.Controllers
 {
     public class HomeController : Controller
     {
+
         // GET: Home
-        
         public ActionResult Index()
         {
             var productosBL = new ProductosBL();
@@ -27,6 +29,40 @@ namespace Looking4Home.Web.Controllers
 
 
             return View(listadeProductos);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string buscar)
+        {
+            return View();
+        }
+
+
+        //GET: Buscar
+        public ActionResult Buscar(SearchModel model)
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos().AsQueryable();  // Marcar como AsQueryable
+
+            var factory = new PageSourceFactory
+            {
+
+                MaxItemsPerPage = 10, // maximo elementos por pagina
+                DefaultItemsPerPage = 9 // elementos por pagina
+            };
+
+            var searchText = model.SearchText;
+            if (!string.IsNullOrWhiteSpace(searchText)) listadeProductos
+                    = listadeProductos.Where(c => c.Descripcion.ToLower().Contains(searchText)); // Buscar en descripcion de la categoria
+
+            var source = factory.CreateSource(listadeProductos, model);
+
+            ViewBag.adminWebsiteUrl =
+                ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+            return View(source);
+
         }
 
     }
